125f9135e827b13a29e3bb6f7336e3465f958cb9
Hermes <hermes@nousresearch.com>
2026-08-23 18:50:16 +0000

    fix: chart tab resets to Chart panel, hide time-bar on non-chart tabs, remove dead padding (v5.3)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hl-terminal-v5.2';
+const CACHE_NAME = 'hl-terminal-v5.3';
 const urlsToCache = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
